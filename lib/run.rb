caf56fdaf8334fe355cda183ccd6c8e5b3667a08
Tuefels Drock <uwjh0808@uw.edu>
2011-11-25 21:11:59 -0800

    x

---
+++ lib/run.rb
@@ -0,0 +1,23 @@
+
+load 'ess.rb'
+
+
+
+fn = 'dummy' # file name 
+
+arg2 = 20 # max number of generations, if not supplied as arg 2
+
+ai = Ess.new(arg2)   
+ai.evolute(fn)
+
+puts ai.so
+
+if ai.winner 
+  # map @winner to choicelist to show winning answer.
+  choicelist = Chl.new
+  choicelist.readCSVFile(fn)  # read in to get the list to map to the winner
+  choicelist.printSolution(ai.winner)
+end
+ 
+
+
